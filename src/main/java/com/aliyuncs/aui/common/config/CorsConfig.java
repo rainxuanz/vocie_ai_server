df929d78c2c70e54c397167d51a7f4cfe902407d
rainxzhangyuxuan <rainxzhangyuxuan@didiglobal.com>
2025-06-08 16:12:31 +0800

    test

--- src/main/java/com/aliyuncs/aui/common/config/CorsConfig.java
+++ src/main/java/com/aliyuncs/aui/common/config/CorsConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 /**
  * 跨域配置
@@ -13,33 +12,7 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        
-        // 允许所有域名进行跨域调用
-        config.addAllowedOriginPattern("*");
-        
-        // 允许所有请求方法
-        config.addAllowedMethod("*");
-        
-        // 允许所有请求头
-        config.addAllowedHeader("*");
-        
-        // 允许携带凭证信息
-        config.setAllowCredentials(true);
-        
-        // 预检请求的缓存时间（秒）
-        config.setMaxAge(3600L);
-        
-        // 添加映射路径，拦截一切请求
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        
-        return new CorsFilter(source);
-    }
-    
-    @Bean
+    @Bean("customCorsConfigurationSource")
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         
